3eeb5f741a2348ed9c158c39a50e6c0fb4ae758f
MaseTheStudent <163777907+MaseTheStudent@users.noreply.github.com>
2024-04-25 16:16:26 +1200

    v6
    added camera feed

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,57 @@
-import React from 'react'
+import React, {useRef, useEffect, useState} from 'react'
 import { Navbar, Brand, CTA } from './components'
 import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers'
 import './App.css';
 const App = () => {
+	// camera references
+	const videoRef = useRef(null);
+	const photoRef = useRef(null);
+
+	const [hasPhoto, setHasPhoto] = useState(false);
+
+	const getVideo = () => (
+		navigator.mediaDevices.getUserMedia({ video: 
+			{ width: 1920/3, height: 1080/3} })
+			.then(stream => {
+				let video =videoRef.current;
+				video.srcObject = stream;
+				video.play();
+			})
+			.catch(err => {
+				console.error(err);
+			})
+	)
+
+	useEffect(() => {
+		getVideo();
+	}, [videoRef]);
+
   return (
-    <div className='App'>
-        <div className='gradient__bg'>
-            <Navbar />
-            <Header />
-        </div>
-        <div className='gradient__bg'>
-            <Brand />
-            
-            <WhatGPT3 />
-            <Features />
-            <Possibility />
-            <CTA />
-            <Blog />
-            <Footer />
-        </div>
-    </div>
+	<div className='App'>
+		<div className='gradient__bg'>
+			<Navbar />
+			<Header />
+		</div>
+		<div className='gradient__bg'>
+			<div className='camera'>
+				<video ref={videoRef}></video>
+				<button>SNAP</button>
+			</div>
+			<div className={'result' + (hasPhoto ?
+			'hasPhoto': '')}>
+				<canvas ref={photoRef}></canvas>
+				<button>CLOSE</button>		
+			</div>
+			<Brand />
+			
+			<WhatGPT3 />
+			<Features />
+			<Possibility />
+			<CTA />
+			<Blog />
+			<Footer />
+		</div>
+	</div>
   )
 }
 
