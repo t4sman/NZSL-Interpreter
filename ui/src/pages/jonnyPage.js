993fa99a4bd689d5e8da4d1711120cef6a75bff0
JohnnyLolo183 <sionezinzan@gmail.com>
2024-05-02 15:53:46 +1200

    Added search bar component, added sign pictures to assers, added a folder 'data' to hold image list.

--- ui/src/pages/jonnyPage.js
+++ ui/src/pages/jonnyPage.js
@@ -1,30 +1,85 @@
-import React from 'react';
-import { Navbar } from '../components';
-import { Header } from '../containers';
+import React, { useState } from 'react';
+import { SearchBar, Navbar } from '../components';
+import imageData from '../data/image';
 
+const JonnyPage = () => {
+  const [searchResult, setSearchResult] = useState(null);
+  const [userImageList, setUserImageList] = useState([]);
 
-const jonnyPage = () => {
+  const handleSearch = (query) => {
+    console.log('Search query:', query);
+
+    const lowerCaseQuery = query.toLowerCase();
+    if (lowerCaseQuery in imageData) {
+      setSearchResult(imageData[lowerCaseQuery]);
+    } else {
+      setSearchResult(null);
+    }
+  };
+
+  const handleAddImage = () => {
+    if (searchResult) {
+      setUserImageList([...userImageList, searchResult]);
+    }
+  };
+
   return (
-    <div className=''>
+    <div>
+      <Navbar />
       <div className='gradient__bg'>
-        <Navbar />
+        <h1>My Learning Pathway</h1>
+
         
-      </div>
-      <div className='gradient__bg'>
-        {/* put your code here */}
+        <p style={{ marginBottom: '10px', textAlign: 'center' }}>Enter your desired image:</p>
+
         
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', marginBottom: '20px' }}>
+          <div style={{ marginRight: '10px' }}>
+            <SearchBar onSearch={handleSearch} />
+          </div>
+          <button
+            onClick={handleAddImage}
+            style={{
+              height: '40px',
+              width: '120px',
+              backgroundColor: 'transparent',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+              fontSize: '16px',
+              fontWeight: 'bold',
+            }}
+          >
+            Add Image
+          </button>
+        </div>
 
+        
+        <div>
+          {searchResult ? (
+            <img src={searchResult} alt="Search Result" style={{ maxWidth: '18%', height: 'auto' }} />
+          ) : (
+            <p style={{ textAlign: 'center' }}>No image found. Please enter a valid search term.</p>
+          )}
+        </div>
 
-
-
-
-
-
-        {/* put your code here */}
+        {/* Display user image list */}
+        <h2>My Sign List</h2>
+        <ul>
+          {userImageList.map((image, index) => (
+            <li key={index}>
+              <img
+                src={image}
+                alt={`User-added object ${index + 1}`}
+                style={{ maxWidth: '18%', height: 'auto' }}
+              />
+            </li>
+          ))}
+        </ul>
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default jonnyPage
+export default JonnyPage;
